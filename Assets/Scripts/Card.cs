483a36cbf5931d18eef23378700c93e536a7d2d5
Sreejit Pillai <pillaisreejit94@gmail.com>
2025-07-29 17:06:03 +0530

    Added flip animations to cards.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    [SerializeField] bool allowClick = false;
     [Header("Card data")]
     [SerializeField] CardData myCardData;
 
@@ -32,8 +33,8 @@ public class Card : MonoBehaviour
     {
         cardContentImage.sprite = myCardData.sprite;
         RescaleUI();
-        Hide();
-        Show();
+        Hide(0f);
+        StartCoroutine(Utils.FadeCanvas(canvasGroup, 0f, 1f, 0.5f));
     }
 
     void RescaleUI()
@@ -44,16 +45,111 @@ public class Card : MonoBehaviour
         }
     }
 
-    void Show()
+    void Show(float duration = 0.5f)
     {
-        canvasGroup.alpha = 1f;
-        hiddenObj.SetActive(false);
-        shownObj.SetActive(true);
+        StopFlippingAnim();
+        StartCoroutine(flippingAnim = FlipToShowRoutine(duration));
     }
 
-    void Hide()
+    void Hide(float duration)
     {
-        hiddenObj.SetActive(true);
-        shownObj.SetActive(false);
+        StopFlippingAnim();
+        StartCoroutine(flippingAnim = FlipToHideRoutine(duration));
     }
+
+    void StopFlippingAnim()
+    {
+        if (flippingAnim != null)
+        {
+            StopCoroutine(flippingAnim);
+        }
+    }
+
+    public void ClickedOnShown()
+    {
+        if (allowClick)
+        {
+            Hide(0.5f);
+        }
+    }
+
+    public void ClickedOnHidden()
+    {
+        if (allowClick)
+        {
+            Show();
+        }
+    }
+
+    IEnumerator flippingAnim;
+    private IEnumerator FlipToShowRoutine(float duration)
+    {
+        allowClick = false;
+        float halfTime = duration / 2f;
+
+        hiddenObj.SetActive(true);
+        hiddenObj.transform.localScale = Vector2.one;
+        hiddenObj.transform.localEulerAngles = Vector3.zero;
+
+        shownObj.SetActive(true);
+        shownObj.transform.localScale = Vector2.zero;
+
+        Quaternion startRotation = hiddenObj.transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, 270, 0);
+        shownObj.transform.rotation = endRotation;
+        hiddenObj.transform.rotation = startRotation;
+
+        yield return Utils.BounceUpEffect(hiddenObj.transform, hiddenObj.transform.localScale);
+        yield return Utils.RotateSlerp(hiddenObj.transform, startRotation, endRotation, halfTime);
+        hiddenObj.transform.rotation = endRotation;
+        hiddenObj.SetActive(false);
+
+        shownObj.transform.localScale = hiddenObj.transform.localScale;
+        yield return Utils.RotateSlerp(shownObj.transform, endRotation, startRotation, halfTime);
+        shownObj.transform.rotation = startRotation;
+        yield return Utils.BounceDownEffect(shownObj.transform, Vector2.one);
+
+        allowClick = true;
+    }
+
+    private IEnumerator FlipToHideRoutine(float duration)
+    {
+        allowClick = false;
+        if (duration <= 0f)
+        {
+            hiddenObj.SetActive(true);
+            shownObj.SetActive(false);
+            hiddenObj.transform.localEulerAngles = Vector3.zero;
+        }
+        else
+        {
+            float halfTime = duration / 2f;
+
+            hiddenObj.SetActive(true);
+            hiddenObj.transform.localScale = Vector2.zero;
+
+            shownObj.SetActive(true);
+            shownObj.transform.localScale = Vector2.one;
+            shownObj.transform.localEulerAngles = Vector3.zero;
+
+            Quaternion startRotation = shownObj.transform.rotation;
+            Quaternion endRotation = startRotation * Quaternion.Euler(0, 270, 0);
+
+            shownObj.transform.rotation = startRotation;
+            hiddenObj.transform.rotation = endRotation;
+
+            yield return Utils.BounceUpEffect(shownObj.transform, shownObj.transform.localScale);
+            yield return Utils.RotateSlerp(shownObj.transform, startRotation, endRotation,halfTime);
+            shownObj.transform.rotation = endRotation;
+            shownObj.SetActive(false);
+
+            hiddenObj.transform.localScale = shownObj.transform.localScale;
+            yield return Utils.RotateSlerp(hiddenObj.transform, endRotation, startRotation,halfTime);
+            hiddenObj.transform.rotation = startRotation;
+            yield return Utils.BounceDownEffect(hiddenObj.transform, Vector2.one);
+        }
+        allowClick = true;
+    }
+    
+
 }
